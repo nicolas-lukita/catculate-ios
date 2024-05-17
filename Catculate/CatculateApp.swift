03a56cabd0197bd64e7fb917496e5eee5655d5b4
Nicolas Lukita <nicolas.lukita@gmail.com>
2024-05-17 23:24:19 +0800

    Initial Commit

---
+++ Catculate/CatculateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatculateApp.swift
+//  Catculate
+//
+//  Created by Nicolas Lukita on 2024/5/17.
+//
+
+import SwiftUI
+
+@main
+struct CatculateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
